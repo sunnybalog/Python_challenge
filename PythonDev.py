42787b68458feb1aa0774656145c92d89fbcf89e
sunnybalog <33362184+sunnybalog@users.noreply.github.com>
2021-05-06 12:49:46 +0100

    Update and rename PythonDev to PythonDev.py

--- PythonDev.py
+++ PythonDev.py
@@ -50,83 +50,68 @@ elif x == 'scissors' and y == 'paper':
 else:
     print('Invalid input: choose either scissors, paper or rock') 
 
+  """
 Modifying loops
-
 Loop control statements are nested inside loops and designed to change their typical behavior. In this topic, we'll find out how they work and what they are used for.
 How to break
-
 The break statement is used to terminate a loop of any type (i. e. for and while loops). It may be said that break "jumps out" of the loop where it was placed. Let’s examine a tiny example:
-
 pets = ['dog', 'cat', 'parrot']
 for pet in pets:
     print(pet)
     if pet == 'cat':
         break
-
 We wanted to stop the loop before it iterated for the last time. For that purpose, we introduced a condition when the loop should be stopped. The output is as follows:
-
 dog
 cat
-
 Be careful where you put print(). If you put it at the loop’s end, the output will return only the first value – ‘dog’. This happens because break exits from the loop immediately.
-
 Often enough, break is used to stop endless while loops like this one:
-
 count = 0
 while True:
     print("I am Infinite Loop")
     count += 1
     if count == 13:
         break
-
 How to continue
-
 The continue operator is commonly used, too. You can stop the iteration if your condition is true and return to the beginning of the loop (that is, jump to the loop's top and continue execution with the next value). Look at the following example:
-
 pets = ['dog', 'cat', 'parrot']
+"""
 for pet in pets:
     if pet == 'dog':
         continue
     print(pet)
-
+"""
 The output will contain all values except the first one ('dog') since it fulfills the condition:
-
 cat
 parrot
-
 Thus, the loop just skips one value and goes on running.
-
 One nuance is worth mentioning: the continue operator should be used moderately. Sometimes you can shorten the code by simply using an if statement with the reversed condition:
-
+"""
 pets = ['dog', 'cat', 'parrot']
 for pet in pets:
     if pet != 'dog':
         print(pet)
-
+"""
 In this case, the output will remain the same:
-
 cat
 parrot
-
 Loop else clause
-
 If the loop didn’t encounter the break statement, an else clause can be used to specify a block of code to be executed after the loop.
-
+"""
+                                                                                                                                           
 pets = ['dog', 'cat', 'parrot']
 for pet in pets:
     print(pet)
 else:
     print('We need a turtle!')
-
+                                                                                                                                           
+"""
 So after the loop body, the else statement will execute:
-
 dog
 cat
 parrot
 We need a turtle!
-
 Importantly, loop else runs if and only if the loop is exited normally (without hitting break). Also, it is run when the loop is never executed (e. g. the condition of the while loop is false right from the start). Consider an example:
-
+"""
 pancakes = 2
 while pancakes > 0:
     print("I'm the happiest human being in the world!")
@@ -136,32 +121,25 @@ while pancakes > 0:
         break
 else:
     print("No pancakes...")
-
+"""
 When we run the code for the first time we'll get this output:
 
 I'm the happiest human being in the world!
 I'm the happiest human being in the world!
 Now I have no pancakes!
+"""
 
+"""
 Execution of the code snippet for the second time (when the condition is not met, for pancakes = 0) will end up with another message:
-
 No pancakes...
-
 In conclusion
-
 To sum up, loop control statements represent a useful tool to alter the way a loop works. You can introduce extra conditions using break, continue and else operators. In addition, they allow you to print a message after the successful code execution, skip a beforehand selected set of values, or even terminate an endless loop. Use them wisely and they'll work wonders.
-
     break:It may help to exit an infinite loop.
-    
-    continue: It skips the current iteration under some specified condition.
-    
+    continue: It skips the current iteration under some specified condition    
 else after a for loop: After a certain number of iterations, it runs an additional code.
-
 else after a while loop: 
 It executes some block of code if the loop's condition is false.
-
-
-
+"""
 
 pancakes = 2
 while pancakes > 0:
@@ -195,7 +173,7 @@ while n_guesses < 5:               # 4
     else:                          # 9
         print('No!')               # 10
     n_guesses += 1                 # 11
-
+"""
 LOOP CONTROL STATEMENTS
 Vowels and consonants
 
@@ -210,7 +188,7 @@ We will use letters from the English alphabet. The symbols aeiou are considered 
 The output format:
 
 Print the line vowel if the character is a vowel, and consonant if the character is consonant. Stop printing information at the first non-alphabetic symbol.
-
+"""
 n_letters = input().lower()
 n_lenght = len(n_letters)
 n_vowels = 'aeiou'
@@ -224,7 +202,7 @@ for next_letter in n_letters:
     else:
         break
     
-
+"""
 CAT CAFES
 Kitty wants to visit a cat café! Help her find the one with the largest number of cats.
 
@@ -233,7 +211,7 @@ Each string contains a café's name followed by a space and the number of cats i
 The names would be one-word only. Read input lines until you get a string MEOW (without any number).
 Output format
 The café with the maximum number of cats.
-
+"""
 items = 0
 cat_cafe = ''
 cat_names = []
@@ -260,17 +238,12 @@ def cafe_name(cafe_names_cats_numbers):
         else:
             break
     return cafe_names_cats_numbers.get(max(cafe_names_cats_numbers.keys()))
-    
-
 dicti = dict()    
 print(cafe_name(dicti))
-
-
+       """
 Calculate the arithmetic mean of integer numbers and output it. You will receive the integers on separate lines. The numeric sequence ends with a period ., so stop reading the input on it.
-
 Don't round your result before outputting it.
-
-The input will always have at least one number. 
+The input will always have at least one number."""
 
 new_number = 0
 summation = 0
